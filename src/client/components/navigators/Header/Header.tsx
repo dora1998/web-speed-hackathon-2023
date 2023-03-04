0e8d009b3f0fe0aaadda41398928da7539072955
dora <31735614+dora1998@users.noreply.github.com>
2023-03-05 01:49:15 +0900

    認証取得待ちのケア

--- src/client/components/navigators/Header/Header.tsx
+++ src/client/components/navigators/Header/Header.tsx
@@ -10,7 +10,7 @@ import { Image } from '../../foundation/Image';
 import * as styles from './Header.styles';
 
 export const Header: FC = () => {
-  const { isAuthUser } = useAuthUser();
+  const { authUserLoading, isAuthUser } = useAuthUser();
   const handleOpenModal = useOpenModal();
 
   return (
@@ -20,21 +20,22 @@ export const Header: FC = () => {
           <Image height={32} src="/icons/logo.png" srcSet="/icons/logo.png, /icons/logo@2x.png 2x" width={205} />
         </div>
       </Anchor>
-      {isAuthUser ? (
-        <Anchor data-testid="navigate-order" href={'/order'}>
-          <div className={styles.orderLink()}>
-            <Icon color="#222222" height={20} icon={FaShoppingCart} width={20} />
-          </div>
-        </Anchor>
-      ) : (
-        <button
-          className={styles.signInButton()}
-          data-testid="navigate-signin"
-          onClick={() => handleOpenModal('SIGN_IN')}
-        >
-          <Icon color="#222222" height={20} icon={FaUser} width={20} />
-        </button>
-      )}
+      {!authUserLoading &&
+        (isAuthUser ? (
+          <Anchor data-testid="navigate-order" href={'/order'}>
+            <div className={styles.orderLink()}>
+              <Icon color="#222222" height={20} icon={FaShoppingCart} width={20} />
+            </div>
+          </Anchor>
+        ) : (
+          <button
+            className={styles.signInButton()}
+            data-testid="navigate-signin"
+            onClick={() => handleOpenModal('SIGN_IN')}
+          >
+            <Icon color="#222222" height={20} icon={FaUser} width={20} />
+          </button>
+        ))}
     </header>
   );
 };
