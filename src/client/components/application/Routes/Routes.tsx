f2018e29ea57cec7e0084855b2bff74d77487650
dora <31735614+dora1998@users.noreply.github.com>
2023-03-05 17:29:17 +0900

    fix(したかった…)

--- src/client/components/application/Routes/Routes.tsx
+++ src/client/components/application/Routes/Routes.tsx
@@ -1,10 +1,13 @@
+import { css } from '@emotion/css';
 import type { FC } from 'react';
 import { lazy } from 'react';
 import * as Router from 'react-router-dom';
 
 import { withAuthorized } from '../../../../server/utils/auth';
 import { GetProductDetailsQuery } from '../../../graphql/queries';
+import { Fallback } from '../../../pages/Fallback';
 import { apolloClient } from '../../../utils/apollo_client';
+import { Header } from '../../navigators/Header';
 import { Layout } from '../Layout';
 
 const NotFound = lazy(() => import('../../../pages/NotFound'));
@@ -14,21 +17,42 @@ const ProductDetail = lazy(() => import('../../../pages/ProductDetail'));
 const Top = lazy(() => import('../../../pages/Top'));
 
 const productDetailLoader: Router.LoaderFunction = async ({ params }) => {
-  await apolloClient.query({
-    query: GetProductDetailsQuery,
-    variables: {
-      productId: parseInt(params.productId ?? ''),
-    },
-  });
+  try {
+    await apolloClient.query({
+      query: GetProductDetailsQuery,
+      variables: {
+        productId: parseInt(params.productId ?? ''),
+      },
+    });
+  } catch (e) {
+    throw new Response('Error', { status: 500 });
+  }
   return null;
 };
 
 const orderLoader: Router.LoaderFunction = withAuthorized(() => null);
 const orderCompleteLoader: Router.LoaderFunction = withAuthorized(() => null);
 
+export const container = () => css`
+  display: grid;
+  height: 50vh;
+  place-items: center;
+`;
+
 const router = Router.createBrowserRouter(
   Router.createRoutesFromElements(
-    <Router.Route element={<Layout />} path="/">
+    <Router.Route
+      element={<Layout />}
+      errorElement={
+        <>
+          <Header />
+          <main className={container()}>
+            <Fallback />
+          </main>
+        </>
+      }
+      path="/"
+    >
       <Router.Route element={<Top />} path="" />
       <Router.Route element={<ProductDetail />} loader={productDetailLoader} path="product/:productId" />
       <Router.Route element={<Order />} loader={orderLoader} path="order" />
