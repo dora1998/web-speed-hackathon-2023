60cbf35f7e5cf27be0ef46746d5fefdf3ecbcdd1
dora <31735614+dora1998@users.noreply.github.com>
2023-03-05 17:09:13 +0900

    遅いクエリをキャッシュでゴリ押しする

--- src/server/graphql/index.ts
+++ src/server/graphql/index.ts
@@ -1,6 +1,7 @@
 import fs from 'node:fs/promises';
 
 import { ApolloServer } from '@apollo/server';
+import responseCachePlugin from '@apollo/server-plugin-response-cache';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
 import type { Context } from '../context';
@@ -37,11 +38,12 @@ export async function initializeApolloServer(): Promise<ApolloServer<Context>> {
       rootResolve('./src/model/zipcode.graphql'),
       rootResolve('./src/server/graphql/mutation.graphql'),
       rootResolve('./src/server/graphql/query.graphql'),
+      rootResolve('./src/server/graphql/cache.graphql'),
     ].map((filepath) => fs.readFile(filepath, { encoding: 'utf-8' })),
   );
 
   const server = new ApolloServer({
-    plugins: [ApolloServerPluginLandingPageLocalDefault({ includeCookies: true })],
+    plugins: [ApolloServerPluginLandingPageLocalDefault({ includeCookies: true }), responseCachePlugin()],
     resolvers: {
       FeatureItem: featureItemResolver,
       FeatureSection: featureSectionResolver,
